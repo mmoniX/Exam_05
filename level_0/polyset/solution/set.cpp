8deb5842a4459ad42f189b5592bdb2f4e65be518
mmoniX <92337206+mmoniX@users.noreply.github.com>
2026-01-08 00:34:42 +0000

    polyset solution done

---
+++ level_0/polyset/solution/set.cpp
@@ -0,0 +1,51 @@
+#include "set.hpp"
+
+set::set(searchable_bag &_bag) : bag(_bag) {}
+
+set::set(const set& copy) : bag(copy.bag) {}
+
+set& set::operator=(const set& copy)
+{
+    if (this != &copy)
+    {
+        bag = copy.bag;
+    }
+    return *this;
+}
+
+set::~set() {}
+
+void set::insert(int value)
+{
+    if (!bag.has(value))
+        bag.insert(value);
+}
+
+void set::insert(int *array, int size);
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (!bag.has(array[i]))
+            bag.insert(array[i]);
+    }
+}
+
+void set::print()
+{
+    bag.print();
+}
+
+void set::clear()
+{
+    bag.clear();
+}
+
+bool set::has(int value) const
+{
+    return bag.has(value);
+}
+
+const searchable_bag& set::get_bag() const
+{
+    return bag;
+}
---
+++ level_0/polyset/solution/set.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "searchable_bag.hpp"
+
+class set
+{
+    private:
+        searchable_bag &bag;
+    public:
+        set(searchable_bag &_bag);
+        set(const set& copy);
+        set& operator=(const set& copy);
+        ~set();
+
+        void insert(int value);
+        void insert(int *array, int size);
+        void print();
+        void clear();
+
+        bool has(int value) const;
+        const searchable_bag& get_bag() const;
+};
