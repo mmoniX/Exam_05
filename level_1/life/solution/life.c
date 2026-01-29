9cf8e8fe0fb931b76e6dd73f36ef6275c5cb90ce
mmoniX <92337206+mmoniX@users.noreply.github.com>
2026-01-29 11:36:45 +0000

    life solution done

--- level_1/life/solution/life.c
+++ level_1/life/solution/life.c
@@ -1,6 +1,4 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "life.h"
 
 int main(int ac, char **av)
 {
---
+++ level_1/life/solution/life.h
@@ -0,0 +1,9 @@
+#ifndef LIFE_H
+# define LIFE_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
